b1a3289fe4b98d1270844aa81f60604e64ae50cf
Slawek Kaplonski <skaplons@redhat.com>
2018-08-27 07:24:00 +0200

    Add connectivity test via 2 routers and tenant networks
    This patch adds test to check if connectivity through 2 routers
    and internal network between routers works fine.
    
    When at least two compute nodes are available for tempest and
    DifferentHostFilter is enabled in nova-scheduler VM1 and VM2 will be
    spawned on two different compute nodes.
    
    Connectivity scenario in test is like on picture below:
    
    	    +-----+
    	    | VM 1|
    	    +--+--+
    	       |
    	       | Network 1 (tenant network)
    	       |
    	     +-+-+
    	     | R1|
    	     +-+-+
    	       |
    	       | Network 3 (tenant network)
    	       |
    	     +-+-+
    	     | R2|
    	     +-+-+
    	       |
    	       | Network 2 (tenant network)
    	       |
    	    +--+--+
    	    | VM 2|
    	    +-----+
    
    Depends-On: https://review.openstack.org/597567
    Change-Id: Ie9903b7078d6fcff307c5d76770d92df1152d473
    Related-Bug: #1786272

--- neutron_tempest_plugin/api/base.py
+++ neutron_tempest_plugin/api/base.py
@@ -670,6 +670,7 @@ class BaseNetworkTest(test.BaseTestCase):
     @classmethod
     def delete_router(cls, router, client=None):
         client = client or cls.client
+        client.remove_router_extra_routes(router['id'])
         body = client.list_router_interfaces(router['id'])
         interfaces = [port for port in body['ports']
                       if port['device_owner'] in const.ROUTER_INTERFACE_OWNERS]
